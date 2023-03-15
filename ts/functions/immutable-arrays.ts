7b43813b39277cff56035a488d7d0dd5868304ba
Alain D'Ettorre <alain.det@gmail.com>
2023-03-15 16:32:20 +0100

    Add arrays methods for immutability

---
+++ ts/functions/immutable-arrays.ts
@@ -0,0 +1,39 @@
+export function immutableInsertAt<T = any>(arr: T[], index: number, item: T): T[] {
+
+  if (index < 0 || index > arr.length - 1) {
+    return arr;
+  }
+
+  return [ ...arr.slice(0, index), item, ...arr.slice(index) ];
+}
+  
+export function immutableUpdateAt<T = any>(arr: T[], index: number | undefined, item: T): T[] {
+
+  if (typeof index === 'undefined' || index < 0 || index > arr.length - 1) {
+    return arr;
+  }
+
+  return [ ...arr.slice(0, index), item, ...arr.slice(index + 1)];
+}
+
+export function immutableUpdateWith<T = any>(arr: T[], index: number, fn: (item: T) => T): T[] {
+  return immutableUpdateAt(arr, index, fn({ ...arr[index] }));
+}
+
+  
+export function immutableRemoveAt<T = any>(arr: T[], index: number): T[] {
+
+  if (index < 0 || index > arr.length - 1) {
+    return arr;
+  }
+
+  return [ ...arr.slice(0, index), ...arr.slice(index + 1) ];
+}
+
+export function immutableSort<T = any>(arr: T[], fn?: (a: T, b: T) => number): T[] {
+  if (sorter) {
+    return [...arr].sort(fn);
+  }
+  
+  return [...arr].sort();
+}
