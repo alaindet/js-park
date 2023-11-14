1487780f222c8f17f1211126fcaa9696c483bb8b
Alain D'Ettorre Aesys <alain.dettorre@aesys.tech>
2023-11-14 16:03:00 +0100

    feat: add WaitGroup example

--- js/experiments/loop-over-promises.js
+++ js/experiments/loop-over-promises.js
@@ -12,7 +12,7 @@ run(); // <-- Start here
 /**
  * Outputs
  * For then example
- * - Parallel-ish, does not wait until finished
+ * # Parallel-ish, does not wait until finished
  * +0.004s Finished.
  * +0.209s a
  * +0.210s b
@@ -22,7 +22,7 @@ run(); // <-- Start here
  * +0.212s f
  *
  * For nested await example
- * - Each iteration blocks the loop (slow!)
+ * # Each iteration blocks the loop (slow!)
  * +0.203s a
  * +0.404s b
  * +0.605s c
@@ -32,7 +32,7 @@ run(); // <-- Start here
  * +1.214s Finished.
  *
  * For await...of example
- * - Parallel-ish, loop wait all promises to settle
+ * # Parallel-ish, loop wait all promises to settle
  * +0.203s a
  * +0.203s b
  * +0.204s c
@@ -40,21 +40,32 @@ run(); // <-- Start here
  * +0.205s e
  * +0.205s f
  * +0.205s Finished.
+ *
+ * For WaitGroup (similar to Go's sync.WaitGroup)
+ * # Parallel-ish, loop wait all promises to settle via WaitGroup
+ * +0.203s a
+ * +0.203s b
+ * +0.204s c
+ * +0.204s d
+ * +0.204s e
+ * +0.204s f
+ * +0.205s Finished.
  */
 
 async function run() {
   await forThenExample();
   await forNestedAwaitExample();
   await forAwaitOfExample();
+  await forWaitGroupExample();
 }
 
 function forThenExample() {
   return new Promise(done => {
     const requestsCount = 5;
     const delay = 200;
-    const { clock, requests } = setupExample(requestsCount, delay);
+    const { clock, requests } = _setupExample(requestsCount, delay);
 
-    printTitleAndDescription(
+    _printTitleAndDescription(
       'For then example',
       'Parallel-ish, does not wait until finished',
     );
@@ -74,9 +85,9 @@ function forThenExample() {
 
 function forNestedAwaitExample() {
   return new Promise(async done => {
-    const { clock, requests } = setupExample(5, 200);
+    const { clock, requests } = _setupExample(5, 200);
 
-    printTitleAndDescription(
+    _printTitleAndDescription(
       'For nested await example',
       'Each iteration blocks the loop (slow!)',
     );
@@ -94,17 +105,17 @@ function forNestedAwaitExample() {
   });
 }
 
-async function forAwaitOfExample() {
+function forAwaitOfExample() {
   return new Promise(async done => {
 
-    const { clock, requests } = setupExample(5, 200);
+    const { clock, requests } = _setupExample(5, 200);
 
-    printTitleAndDescription(
+    _printTitleAndDescription(
       'For await...of example',
       'Parallel-ish, loop wait all promises to settle',
     );
 
-     // INTERESTING STARTS HERE
+    // INTERESTING STARTS HERE
     for await (const response of requests.map(request => request())) {
       clock.track(response);
     }
@@ -116,15 +127,44 @@ async function forAwaitOfExample() {
   });
 }
 
-function setupExample(requestsCount = 5, delay = 200) {
-  const clock = createClock();
+function forWaitGroupExample() {
+  return new Promise(async done => {
+
+    const wg = _createWaitGroup();
+    const { clock, requests } = _setupExample(5, 200);
+
+    _printTitleAndDescription(
+      'For WaitGroup (similar to Go\'s sync.WaitGroup)',
+      'Parallel-ish, loop wait all promises to settle via WaitGroup',
+    );
+
+    // INTERESTING STARTS HERE
+    wg.add(requests.length);
+
+    for (const request of requests) {
+      request().then(res => {
+        clock.track(res);
+        wg.done();
+      });
+    }
+
+    await wg.wait();
+    clock.end();
+    // INTERESTING ENDS HERE
+
+    done();
+  });
+}
+
+function _setupExample(requestsCount = 5, delay = 200) {
+  const clock = _createClock();
   clock.start();
-  const requests = createFakeRequests(requestsCount, delay);
+  const requests = _createFakeRequests(requestsCount, delay);
 
   return { requests, clock };
 }
 
-function createFakeRequests(requestsCount = 3, delay) {
+function _createFakeRequests(requestsCount = 3, delay) {
 
   const ALPHABET = 'abcdefghijklmnopqrstuvz';
 
@@ -135,20 +175,20 @@ function createFakeRequests(requestsCount = 3, delay) {
     const letterIndex = i % ALPHABET.length;
     const letter = ALPHABET[letterIndex];
     const message = letter.repeat(round);
-    const request = createFakeRequest(message, delay);
+    const request = _createFakeRequest(message, delay);
     requests.push(request);
   }
 
   return requests;
 }
 
-function createFakeRequest(message, delay) {
+function _createFakeRequest(message, delay) {
   return () => new Promise(done => {
     setTimeout(() => done(message), delay);
   });
 }
 
-function createClock() {
+function _createClock() {
 
   let startedOn = null;
 
@@ -174,7 +214,72 @@ function createClock() {
   return { start, end, track };
 }
 
-function printTitleAndDescription(title, description) {
+function _printTitleAndDescription(title, description) {
   console.log(`\n${title}`);
-  console.log(`- ${description}`);
+  console.log(`# ${description}`);
+}
+
+function _createWaitGroup() {
+
+	let workers = 0;
+	let queue;
+
+	function add(workersToAdd = 1) {
+
+		if (workersToAdd < 0) {
+			throw new Error('Cannot provide negative number of workers');
+		}
+
+		workers += workersToAdd;
+
+		if (!workers) {
+			dequeue();
+		}
+	}
+
+	function done() {
+		if (!workers) {
+			throw new Error('No worker remaining');
+		}
+
+		workers--;
+
+		if (!workers) {
+			dequeue();
+		}
+	}
+
+	function wait() {
+		return new Promise(enqueue);
+	}
+
+	function enqueue(done) {
+
+		if (!workers) {
+			done();
+			return;
+		}
+
+		if (queue === undefined) {
+			queue = [];
+		}
+
+		queue.push(done);
+	}
+
+	function dequeue() {
+		while (queue.length) {
+			const workerFn = queue.shift();
+
+			if (workerFn) {
+				workerFn();
+			}
+		}
+	}
+
+	return {
+		add,
+		done,
+		wait,
+	};
 }
